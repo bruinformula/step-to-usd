33a4766340a87d1081634d78b0357a5c2f58ecaf
Owen O'Malley <theowen@email.com>
2026-02-12 00:34:04 -0800

    Seperated lookdev stuff and to be geom stuff

---
+++ src/geom/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+// Gmsh
+#include <gmsh.h>
+
+// OpenUSD
+#include <pxr/usd/usd/stage.h>
+#include <pxr/usd/usdGeom/xform.h>
+#include <pxr/base/tf/token.h>
+
+int main(int argc, char** argv) {
+    // gmsh test 
+    gmsh::initialize();
+    gmsh::model::add("dummy_model");
+
+    int pointTag = gmsh::model::geo::addPoint(0.0, 0.0, 0.0);
+    gmsh::model::geo::synchronize();
+
+    std::cout << "Created Gmsh point with tag: " << pointTag << std::endl;
+
+    gmsh::finalize();
+
+    // usd test 
+    pxr::UsdStageRefPtr stage = pxr::UsdStage::CreateInMemory();
+
+    pxr::UsdGeomXform xform =
+        pxr::UsdGeomXform::Define(stage, pxr::SdfPath("/Root"));
+
+    std::cout << "Created USD stage and Xform: "
+                << xform.GetPath().GetString() << std::endl;
+
+    std::cout << "Dummy test completed successfully." << std::endl;
+    return 0;
+}
